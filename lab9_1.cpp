9ae284bbd7ecf18c22a560e0da7442ab7ee5d923
Buburry <Natapon7934@gmail.com>
2023-12-15 16:17:47 +0700

    lab9.1 complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan,rate_year,per_year,interest;  
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate_year;
 	cout << "Enter amount you can pay per year: ";
+	cin >> per_year;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,38 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
+	/*cout << fixed << setprecision(2); 
 	cout << setw(13) << left << 1; 
 	cout << setw(13) << left << 1000.0;
 	cout << setw(13) << left << 50.0;
 	cout << setw(13) << left << 1050.0;
 	cout << setw(13) << left << 100.0;
 	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << "\n";	*/
+	
+	int i=1;
+	do{
+		cout << setw(13) << left << fixed << setprecision(2) << i++; //EndOfYear#
+		cout << setw(13) << left << fixed << setprecision(2) << loan; //PrevBalance
+		interest = loan * (rate_year/100);
+		cout << setw(13) << left << fixed << setprecision(2) << interest;//Interest ดอกเบี้ยของปีนี้ที่คำนวณจากยอดหนี้ยกมาจากปีก่อน x (อัตราดอกเบี้ย/100)
+		loan += interest;
+		cout << setw(13) << left << fixed << setprecision(2) << loan; //total
+
+		if (loan > per_year)  //Payment  
+		{
+			cout << setw(13) << left << fixed << setprecision(2) << per_year;	
+		} else {
+			cout << setw(13) << left << fixed << setprecision(2) << loan;	
+		}
+		loan = loan - per_year;
+		if(loan > 0){
+			cout << setw(13) << left << fixed << setprecision(2) << loan;	//Newbalance
+		} else {
+			cout << setw(14) << left << "0.00";
+		}
+		cout << "\n";						
+	} while (loan > 0);
 	
 	return 0;
 }
